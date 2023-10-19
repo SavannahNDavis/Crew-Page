9f92be5c02463a1578b00d356444bb9e13e7cd42
snovacain <61154743+snovacain@users.noreply.github.com>
2023-10-19 11:10:40 -0700

    light CSS changes

--- crew.js
+++ crew.js
@@ -3,7 +3,7 @@ const aboutCrewEl = document.querySelector('.crew_description')
 
 let showDescription = () => {
 
-    if (aboutCrewEl.style.display === 'none') {
+    if (aboutCrewEl.style.display != 'block') {
         return aboutCrewEl.style.display = "block"
     } else {
         return aboutCrewEl.style.display = "none"
